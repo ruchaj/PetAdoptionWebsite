d310c0259eedb65ebc2a2ca0898ff6a3e17f9bba
mtt37 <tsujim@seattleu.edu>
2023-04-19 17:19:19 -0700

    I added comments to describe the pet listing and blog pages.

---
+++ src/Pages/PetMap.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    public class PetMapModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
