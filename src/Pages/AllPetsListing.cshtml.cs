7069da832dae2f9c4acd0fd36c2e1037b8301ccb
mtt37 <tsujim@seattleu.edu>
2023-04-19 17:14:00 -0700

    I added a page to contain all available pets called AllPetsListing.

---
+++ src/Pages/AllPetsListing.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    public class AllPetsListingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
