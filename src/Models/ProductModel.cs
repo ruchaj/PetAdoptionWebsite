eeeeaebfc08be9499973404be0b5b256489a95d8
lesli <ug@fontbonne.edu>
2023-04-24 21:34:22 -0700

    the old product model got changed to the new product model file(ProductModel.cs)

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -6,17 +6,20 @@ namespace ContosoCrafts.WebSite.Models
     public class ProductModel
     {
         public string Id { get; set; }
-        public string Maker { get; set; }
-        
+        public string Name { get; set; }
+        public string Age { get; set; }
+
         [JsonPropertyName("img")]
         public string Image { get; set; }
-        public string Url { get; set; }
-        public string Title { get; set; }
+
+        public string Breed { get; set; }
+        public string Location { get; set; }
         public string Description { get; set; }
+        public string Cost { get; set; }
         public int[] Ratings { get; set; }
 
         public override string ToString() => JsonSerializer.Serialize<ProductModel>(this);
 
- 
+
     }
 }
