d4e4d8b524bb51a1d8e2f24213121f02594ff6e4
Michiru Tsuji <tsujim@seattleu.edu>
2023-04-26 17:35:59 -0700

    MT: Added the OnGet method to the AllPetsListing page.

--- src/Pages/AllPetsListing.cshtml.cs
+++ src/Pages/AllPetsListing.cshtml.cs
@@ -9,6 +9,7 @@ namespace ContosoCrafts.WebSite.Pages
     public class AllPetsListingModel : PageModel
     {
         private readonly ILogger<AllPetsListingModel> _logger;
+        public JsonFileProductService PetService;
 
         public AllPetsListingModel(ILogger<AllPetsListingModel> logger,
             JsonFileProductService petService)
