5335f343f194eb7653ab9285236ff83cda5b1c6e
Michiru Tsuji <tsujim@seattleu.edu>
2023-04-26 17:29:39 -0700

    MT: Added a logger to the AllPetsListing page.

--- src/Pages/AllPetsListing.cshtml.cs
+++ src/Pages/AllPetsListing.cshtml.cs
@@ -1,12 +1,18 @@
+using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
 
 namespace ContosoCrafts.WebSite.Pages
 {
     public class AllPetsListingModel : PageModel
     {
-        public void OnGet()
+        private readonly ILogger<AllPetsListingModel> _logger;
+
+        public AllPetsListingModel(ILogger<AllPetsListingModel> logger)
         {
+            _logger = logger;
         }
     }
 }
