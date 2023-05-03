4db4f60b54f91fcca7f12c75271b705d6b4351ae
Chanyanuch Somprakit <csomprakit@seattleu.edu>
2023-05-02 21:59:06 -0700

    Added Update.cshtmlcs to the Product folder

---
+++ src/Pages/Product/Update.cshtml.cs
@@ -0,0 +1,58 @@
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq;
+
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    public class UpdateModel : PageModel
+    {
+
+        // Data middletier
+        public JsonFileProductService ProductService { get; }
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="productService"></param>
+        public UpdateModel(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        // The data to show, bind to it for the post
+        [BindProperty]
+        public ProductModel Product { get; set; }
+
+        /// <summary>
+        /// Load data
+        /// </summary>
+        /// <param name="id"></param>
+        public void OnGet(string id)
+        {
+            Product = ProductService.GetProducts().FirstOrDefault(m => m.Id.Equals(id));
+        }
+
+        /// <summary>
+        /// Post the model back to the page
+        /// Update data, then return to the pet listing page
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult OnPost()
+        {
+            //Check if state is valid
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            //Call update data function
+            ProductService.UpdateData(Product);
+
+            //Return to pet listing page
+            return RedirectToPage("./AllPetsListing");
+        }
+    }
+}
