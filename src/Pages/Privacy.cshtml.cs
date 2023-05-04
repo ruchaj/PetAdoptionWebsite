bea1daa10d854730ffe5b9c33693ea220483345b
ruchaj <ruchajrocks@gmail.com>
2023-05-03 21:56:24 -0700

    Added comments to Privacy.cshtml.cs

--- src/Pages/Privacy.cshtml.cs
+++ src/Pages/Privacy.cshtml.cs
@@ -5,12 +5,20 @@ namespace ContosoCrafts.WebSite.Pages
 {
     public class PrivacyModel : PageModel
     {
+
+        /// <summary>
+        /// defines class with a logger that checks whenever its accessed.
+        /// </summary>
         private readonly ILogger<PrivacyModel> _logger;
 
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
             _logger = logger;
         }
+
+        /// <summary>
+        /// Gets the privacy statement
+        /// </summary>
 
         public void OnGet()
         {
