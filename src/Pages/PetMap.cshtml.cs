cd4b27fbd3c3b2c3e998909331d98ad0273f85f4
ruchaj <ruchajrocks@gmail.com>
2023-05-03 21:55:26 -0700

    Adds comments to PetMap.cshtml.cs

--- src/Pages/PetMap.cshtml.cs
+++ src/Pages/PetMap.cshtml.cs
@@ -5,6 +5,9 @@ namespace ContosoCrafts.WebSite.Pages
 {
     public class PetMapModel : PageModel
     {
+        /// <summary>
+        /// Gets the map of pets/pet adoption centers in the area specified.
+        /// </summary>
         public void OnGet()
         {
         }
