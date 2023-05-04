c40cedb96dedfacbe729fd4a395399bdb677ebc4
lesli <ug@fontbonne.edu>
2023-05-03 22:56:17 -0700

    Added test case to Read.cshtml.Tests.cs

---
+++ UnitTests/Pages/Product/Read.cshtml.Tests.cs
@@ -0,0 +1,39 @@
+﻿
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages.Product;
+
+namespace UnitTests.Pages.Product.Read
+{
+    public class ReadTests
+    {
+        #region TestSetup
+        public static PetInformation pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new PetInformation(TestHelper.ProductService)
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Should_Return_Products()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet("1");
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual("Playful,  Socialable, Curiosity", pageModel.Product.Description);
+        }
+        #endregion OnGet
+    }
+}
