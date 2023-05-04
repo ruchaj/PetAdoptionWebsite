b2fd693d15771a2df16ddedd04b58cf910207e0a
Michiru Tsuji <tsujim@seattleu.edu>
2023-05-03 19:43:17 -0700

    MT: Added methods to Create.cshtml.cs.

--- src/Pages/Product/Create.cshtml.cs
+++ src/Pages/Product/Create.cshtml.cs
@@ -1,12 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+
 namespace ContosoCrafts.WebSite.Pages.Product
 {
     public class CreateModel : PageModel
     {
-        public void OnGet()
+        // Data middle tier
+        public JsonFileProductService ProductService { get; }
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="productService"></param>
+        public CreateModel(JsonFileProductService productService)
         {
+            ProductService = productService;
+        }
+
+        // The data to show.
+        public ProductModel Product;
+        public IActionResult OnGet()
+        {
+            Product = ProductService.CreateData();
+            return RedirectToPage("./Update", new { Id = Product.Id });
         }
     }
 }
