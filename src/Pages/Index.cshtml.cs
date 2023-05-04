f39e23667ae5ba7c3c1641260be49015dba6f630
ruchaj <ruchajrocks@gmail.com>
2023-05-03 21:51:56 -0700

    Adds comments to Index.cshtml.cs

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -5,13 +5,19 @@ using Microsoft.Extensions.Logging;
 
 using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Services;
-//Comment comment
 namespace ContosoCrafts.WebSite.Pages
 {
     public class IndexModel : PageModel
     {
+        //Logs whenever the IndexModel is spawned
         private readonly ILogger<IndexModel> _logger;
 
+
+        /// <summary>
+        ///Adds a logger and a product service to every indexmodel instance
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="productService"></param>
         public IndexModel(ILogger<IndexModel> logger,
             JsonFileProductService productService)
         {
@@ -19,9 +25,19 @@ namespace ContosoCrafts.WebSite.Pages
             ProductService = productService;
         }
 
+        /// <summary>
+        /// Gets the product Service
+        /// </summary>
         public JsonFileProductService ProductService { get; }
+
+        /// <summary>
+        /// Gets all the products and sets them, but only in private/specific instances
+        /// </summary>
         public IEnumerable<ProductModel> Products { get; private set; }
 
+        /// <summary>
+        /// Gets all the products in Index
+        /// </summary>
         public void OnGet()
         {
             Products = ProductService.GetProducts();
