3eb6c64eff87f9384b6e7a1139542703758ec67b
ruchaj <ruchajrocks@gmail.com>
2023-05-03 21:57:47 -0700

    Adds comments to Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -5,11 +5,20 @@ namespace ContosoCrafts.WebSite
 {
     public class Program
     {
+        /// <summary>
+        /// Main function that builds and runs all of the ContosoCrafts website (basic)
+        /// </summary>
+        /// <param name="args"></param>
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
+        /// <summary>
+        /// Configures the program by using the information processed in startup.
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
