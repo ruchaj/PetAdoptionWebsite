1f6dc482bc712271cb7995df0cdafa0304f9970e
Chanyanuch Somprakit <csomprakit@seattleu.edu>
2023-05-08 21:30:47 -0700

    Added an AboutUs.cshtml.cs page

---
+++ src/Pages/AboutUs.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    public class AboutUsModel : PageModel
+    {
+        /// <summary>
+        /// Gets the map of pets/pet adoption centers in the area specified.
+        /// </summary>
+        public void OnGet()
+        {
+        }
+    }
+}
