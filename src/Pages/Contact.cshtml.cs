ea1c5ccc62637f37f4550547d0b7067158b1e8b8
Chanyanuch Somprakit <csomprakit@seattleu.edu>
2023-05-08 19:06:35 -0700

    Added a Contact.cshtml.cs page

---
+++ src/Pages/Contact.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    public class ContactModel : PageModel
+    {
+
+        /// <summary>
+        /// defines class with a logger that checks whenever its accessed.
+        /// </summary>
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public ContactModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Gets the Contact information
+        /// </summary>
+        public void OnGet()
+        {
+        }
+    }
+}
