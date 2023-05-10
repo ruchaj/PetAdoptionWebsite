553c1c089d03d03ce4e0941028088b5af22ba370
Michiru Tsuji <tsujim@seattleu.edu>
2023-05-10 00:37:09 -0700

    MT: Added unit test for controller.

--- UnitTests/Controllers/ProductsController.Tests.cs
+++ UnitTests/Controllers/ProductsController.Tests.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using NUnit.Framework;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using ContosoCrafts.WebSite.Controllers;
 
 namespace UnitTests.Controllers
 {
-    internal class ProductsController
+    /// <summary>
+    /// A class to test the ProductsController.cs page.
+    /// </summary>
+    public class ProductsControllerTests
     {
+        #region TestSetup
+        /// <summary>
+        /// Initialize the tests herein.
+        /// </summary>
+        public void TestInitialize()
+        {
+
+        }
+        #endregion TestSetup
+
+        #region
+        [Test]
+        public void get_AllData_Present_Should_Return_True()
+        {
+            // Arrange
+            // Act
+            var newData = new ProductsController(TestHelper.ProductService).Get().First();
+
+            var response = TestHelper.ProductService.GetProducts().First();
+
+            // Assert
+            Assert.AreEqual(newData.Id, response.Id);
+        }
+        #endregion
     }
 }
