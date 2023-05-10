a864e96e4f631b8c8bbae178f6296ad61419950a
Michiru Tsuji <tsujim@seattleu.edu>
2023-05-10 01:24:18 -0700

    MT: Commenting the Read unit test.

--- UnitTests/Pages/Product/Read.cshtml.Tests.cs
+++ UnitTests/Pages/Product/Read.cshtml.Tests.cs
@@ -1,16 +1,22 @@
-﻿
-
-using NUnit.Framework;
-
+﻿using NUnit.Framework;
 using ContosoCrafts.WebSite.Pages.Product;
 
 namespace UnitTests.Pages.Product.Read
 {
+    /// <summary>
+    /// A class to test Read.
+    /// </summary>
     public class ReadTests
     {
         #region TestSetup
+        /// <summary>
+        /// Generates a new model for testing.
+        /// </summary>
         public static PetInformation pageModel;
 
+        /// <summary>
+        /// Initializes the tests herein.
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
@@ -18,10 +24,12 @@ namespace UnitTests.Pages.Product.Read
             {
             };
         }
-
         #endregion TestSetup
 
         #region OnGet
+        /// <summary>
+        /// Testing the OnGet method.
+        /// </summary>
         [Test]
         public void OnGet_Valid_Should_Return_Products()
         {
