4901177e9f9286a8b26ef810a5032aa788cc36dc
Michiru Tsuji <tsujim@seattleu.edu>
2023-05-10 00:43:31 -0700

    MT: Added more unit test for Product Controller.

--- UnitTests/Controllers/ProductsController.Tests.cs
+++ UnitTests/Controllers/ProductsController.Tests.cs
@@ -20,17 +20,39 @@ namespace UnitTests.Controllers
         #endregion TestSetup
 
         #region
+        /// <summary>
+        /// Tests that all present data returns true.
+        /// </summary>
         [Test]
         public void get_AllData_Present_Should_Return_True()
         {
             // Arrange
+
             // Act
             var newData = new ProductsController(TestHelper.ProductService).Get().First();
-
             var response = TestHelper.ProductService.GetProducts().First();
 
             // Assert
             Assert.AreEqual(newData.Id, response.Id);
+        }
+        #endregion
+
+        #region
+        /// <summary>
+        /// Tests patch for adding a valid rating.
+        /// </summary>
+        [Test]
+        public void Patch_AddValid_Rating_Should_Return_True()
+        {
+            // Arrange
+
+            // Act
+            var newData = new ProductsController(TestHelper.ProductService);
+            var newRating = new ProductsController.RatingRequest();
+            newData.Patch(newRating);
+
+            // Assert
+            Assert.AreEqual(newData.ProductService.GetProducts().Last().Id, newRating.ProductId);
         }
         #endregion
     }
