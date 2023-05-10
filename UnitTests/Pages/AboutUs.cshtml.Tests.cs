4cc3b4eae3bb0fa32116eb4bb67d3b0e5c9226da
Michiru Tsuji <tsujim@seattleu.edu>
2023-05-09 23:04:16 -0700

    MT: Added an OnGet unit test to the AboutUs tests, using the Privacy OnGet unit test as a reference.

--- UnitTests/Pages/AboutUs.cshtml.Tests.cs
+++ UnitTests/Pages/AboutUs.cshtml.Tests.cs
@@ -15,5 +15,39 @@ namespace UnitTests.Pages.AboutUs
     /// </summary>
     public class AboutUsTests
     {
+        #region TestSetup
+        public static AboutUsModel pageModel;
+
+        /// <summary>
+        /// Set up the tests herein.
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            var MockLoggerDirect = Mock.Of<ILogger<AboutUsModel>>();
+
+            pageModel = new AboutUsModel()
+            {
+                PageContext = TestHelper.PageContext,
+                TempData = TestHelper.TempData,
+            };
+        }
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Activity_ShouldReturn_True()
+        {
+            // Arrange
+            Activity activity = new Activity("activity");
+            activity.Start();
+
+            // Act
+            pageModel.OnGet();
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+        }
+        #endregion OnGet
     }
 }
