f9e7902974d5fb692e83011d7e81958551169cde
Michiru Tsuji <tsujim@seattleu.edu>
2023-05-09 23:14:09 -0700

    MT: Added constructor to AboutUs.cshtml.cs.

--- src/Pages/AboutUs.cshtml.cs
+++ src/Pages/AboutUs.cshtml.cs
@@ -1,10 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
     public class AboutUsModel : PageModel
     {
+        /// <summary>
+        /// Defines class with a logger that checks whenever it is accessed.
+        /// </summary>
+        private readonly ILogger<AboutUsModel> _logger;
+
+        /// <summary>
+        /// Constructor for the AboutUSModel.
+        /// </summary>
+        /// <param name="logger"></param>
+        public AboutUsModel(ILogger<AboutUsModel> logger)
+        {
+            _logger = logger;
+        }
+
         /// <summary>
         /// Gets the map of pets/pet adoption centers in the area specified.
         /// </summary>
