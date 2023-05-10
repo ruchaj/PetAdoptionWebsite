62ab3652ee83c1a7d44e113799fc09836a207d21
Michiru Tsuji <tsujim@seattleu.edu>
2023-05-09 23:53:33 -0700

    MT: Added constructor and comments to PetMap page.

--- src/Pages/PetMap.cshtml.cs
+++ src/Pages/PetMap.cshtml.cs
@@ -1,10 +1,23 @@
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContosoCrafts.WebSite.Pages
 {
     public class PetMapModel : PageModel
     {
+        /// <summary>
+        /// Defines class with a logger that checks whenever it is accessed.
+        /// </summary>
+        private readonly ILogger<PetMapModel> _logger;
+
+        /// <summary>
+        /// Constructor for the PetMapModel.
+        /// </summary>
+        /// <param name="logger"></param>
+        public PetMapModel(ILogger<PetMapModel> logger)
+        {
+            _logger = logger;
+        }
         /// <summary>
         /// Gets the map of pets/pet adoption centers in the area specified.
         /// </summary>
