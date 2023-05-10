6a93006afa732ab4262361cef450d6491774df87
Michiru Tsuji <tsujim@seattleu.edu>
2023-05-09 23:35:58 -0700

    MT: Created the BlogAndStories unit test setup and OnGet, using Privacy as a reference.

--- UnitTests/Pages/BlogAndStories.cshtml.Tests.cs
+++ UnitTests/Pages/BlogAndStories.cshtml.Tests.cs
@@ -1,12 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 
-namespace UnitTests.Pages
+using Microsoft.Extensions.Logging;
+
+using NUnit.Framework;
+
+using Moq;
+
+using ContosoCrafts.WebSite.Pages;
+
+namespace UnitTests.Pages.BlogAndStories
 {
-    internal class BlogAndStories
+    /// <summary>
+    /// A class to test the BlogAndStories.cshtml.cs page.
+    /// </summary>
+    public class BlogAndStoriesTests
     {
+        /// <summary>
+        /// Create a public page model for BlogAndStories.
+        /// </summary>
+        #region TestSetup
+        public static BlogAndStoriesModel pageModel;
+
+        /// <summary>
+        /// Set up the tests herein.
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            var MockLoggerDirect = Mock.Of<ILogger<BlogAndStoriesModel>>();
+
+            pageModel = new BlogAndStoriesModel(MockLoggerDirect)
+            {
+                PageContext = TestHelper.PageContext,
+                TempData = TestHelper.TempData,
+            };
+        }
+        #endregion TestSetup
+
+        #region OnGet
+        /// <summary>
+        /// Tests the OnGet method of the BlogAndStories page.
+        /// </summary>
+        [Test]
+        public void OnGet_Valid_Activity_ShouldReturn_True()
+        {
+            // Arrange
+            Activity activity = new Activity("activity");
+            activity.Start();
+
+            // Act
+            pageModel.OnGet();
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+        }
+        #endregion OnGet
     }
 }
