a9e01f3ee948d2b5eb59ac4f9f6b7b49fde94bca
Michiru Tsuji <tsujim@seattleu.edu>
2023-05-09 23:22:08 -0700

    MT: Added documentation and a constructor to BlogAndStories.

--- src/Pages/BlogAndStories.cshtml.cs
+++ src/Pages/BlogAndStories.cshtml.cs
@@ -1,11 +1,27 @@
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContosoCrafts.WebSite.Pages
 {
     public class BlogAndStoriesModel : PageModel
     {
-        //Gets Stories of successful pet adoptions
+        /// <summary>
+        /// Defines class with a logger that checks whenever it is accessed.
+        /// </summary>
+        private readonly ILogger<BlogAndStoriesModel> _logger;
+
+        /// <summary>
+        ///  Constructor for the BlogAndStoriesModel.
+        /// </summary>
+        /// <param name="logger"></param>
+        public BlogAndStoriesModel(ILogger<BlogAndStoriesModel> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Gets stories of successful pet adoptions.
+        /// </summary>
         public void OnGet()
         {
         }
