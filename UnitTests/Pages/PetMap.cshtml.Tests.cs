da79bb8e4a0224ca2f3b63dfaf4a5e2b839cd883
Michiru Tsuji <tsujim@seattleu.edu>
2023-05-09 23:56:51 -0700

    MT: Added setup to PetMap unit tests.

--- UnitTests/Pages/PetMap.cshtml.Tests.cs
+++ UnitTests/Pages/PetMap.cshtml.Tests.cs
@@ -1,11 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 
-namespace UnitTests.Pages
+using Microsoft.Extensions.Logging;
+
+using NUnit.Framework;
+
+using Moq;
+
+using ContosoCrafts.WebSite.Pages;
+
+namespace UnitTests.Pages.PetMap
 {
+    /// <summary>
+    /// A class to test the PetMap.cshtml.cs page.
+    /// </summary>
+    public class PetMapTests
+    {
+        #region TestSetup
+        /// <summary>
+        /// Create a public page model for PetMap.
+        /// </summary>
+        public static PetMapModel pageModel;
+
+        /// <summary>
+        /// Set up the tests herein.
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+
+        }
+        #endregion TestSetup
+    }
     internal class PetMap
     {
     }
