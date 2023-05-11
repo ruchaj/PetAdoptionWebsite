c9b19fd1aadd835e69db7c6753ffc724891b5a0f
lesli <ug@fontbonne.edu>
2023-05-10 21:51:44 -0700

    Added comments to Survey.cshtml.cs

--- src/Pages/Survey.cshtml.cs
+++ src/Pages/Survey.cshtml.cs
@@ -7,38 +7,50 @@ namespace ContosoCrafts.WebSite.Pages
 {
     public class SurveyModel : PageModel
     {
+        //Define what the final result is after the users take the survey
         public string Message { get; set; }
+
+        //read the answer for question 1 (checked radio) from the cshtml file
         [BindProperty(Name = "q1", SupportsGet = true)]
         public string Question1Answer { get; set; }
 
+        //read the answer for question 2 (checked radio) from the cshtml file
         [BindProperty(Name = "q2", SupportsGet = true)]
         public string Question2Answer { get; set; }
 
+        //read the answer for question 3 (checked radio) from the cshtml file
         [BindProperty(Name = "q3", SupportsGet = true)]
         public string Question3Answer { get; set; }
 
+        //read the answer for question 4 (checked radio) from the cshtml file
         [BindProperty(Name = "q4", SupportsGet = true)]
         public string Question4Answer { get; set; }
 
+        //read the answer for question 5 (checked radio) from the cshtml file
         [BindProperty(Name = "q5", SupportsGet = true)]
         public string Question5Answer { get; set; }
 
 
+        //place holder for question 1
         public string Question1 { get; set; } = "1. In the event of unexpected expenses, " +
                                                 "what is the maximum amount you are willing to" +
                                                 " spend annually on your pet?";
 
+        //place holder for question 2
         public string Question2 { get; set; } = "2. How much time are you willing to spend" +
                                                 " cleaning up after your pets each day/week without " +
                                                 "feeling frustrated?";
 
+        //place holder for question 3
         public string Question3 { get; set; } = "3. How much time can you dedicate to spending with your" +
                                                 " pets after a busy day?";
 
+        //place holder for question 4
         public string Question4 { get; set; } = "4. Are you comfortable allowing your pet to roam free" +
                                                 " in your living space or do they need to be kept in " +
                                                 "a cage or tank?";
 
+        //place holder for question 5
         public string Question5 { get; set; } = "5. What is the reason behind your desire to have a pet?";
 
 
@@ -47,8 +59,13 @@ namespace ContosoCrafts.WebSite.Pages
             //nothing to do here for now
         }
 
+
+        /// <summary>
+        /// A class to handle the submit action
+        /// </summary>
         public void OnPost()
         {
+            //assign message value with the responses.
             var message = $"{Question1Answer}," +
                             $"{Question2Answer}," +
                             $"{Question3Answer}," +
@@ -56,12 +73,16 @@ namespace ContosoCrafts.WebSite.Pages
                             $"{Question5Answer}";
 
             
-
+            //string array to store all the animals from the response
             string[] responses = message.Split(',');
+
+            //define the point
             int point = GetPoint(responses);
 
+            //initialize result as null value
             string result = null;
 
+            //result is displayed based on point values.
             if(point <= 25)
             {
                 result = $"Your perfect pet can be a fish or a lot of fishes! They are cute and very easy to take care of";
@@ -83,14 +104,22 @@ namespace ContosoCrafts.WebSite.Pages
                 result = $"Your perfect pet can be a big dog! They are very protective and loyal! They are a great companion!";
             }
 
+            //assign Message with result
             Message = result;
 
         
         }
 
+
+        /// <summary>
+        /// A class to count the final point.
+        /// </summary>
         public int GetPoint(string[] responses)
         {
+            //initilize points and set it to zero
             int point = 0;
+
+            //run through the whole array and sum up the points
             for(int i =0; i < responses.Length; i++)
             {
                 if (responses[i] == "smallfish")
@@ -123,6 +152,7 @@ namespace ContosoCrafts.WebSite.Pages
                 }
             }
 
+            //return the point value
             return point;
         }
     }
