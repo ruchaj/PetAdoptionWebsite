cd15cb3efbb9c891822683638daa7d546fd33a96
Michiru Tsuji <tsujim@seattleu.edu>
2023-05-17 14:46:51 -0700

    Added a test to ProductList.razor.Tests.cs

--- UnitTests/Components/ProductList.razor.Tests.cs
+++ UnitTests/Components/ProductList.razor.Tests.cs
@@ -18,6 +18,20 @@ namespace UnitTests.Components
         public void TestInitialize() { }
 
         #endregion TestSetup
+        [Test]
+        public void SelectProduct_Valid_ID_1_Should_Return_Content()
+        {
+            // Arrange
+            Services.AddSingleton<JsonFileProductService>(TestHelper.ProductService);
+            var id = "MoreInfoButton_1";
+            var page = RenderComponent<ProductList>();
+
+            // Find the Button (more info)
+            var buttonList = page.FindAll("Button");
+
+            // Find the pet that matches the ID looking for and click it
+            var button = buttonList.First(m => m.Outer)
+        }
 
     }
 }
