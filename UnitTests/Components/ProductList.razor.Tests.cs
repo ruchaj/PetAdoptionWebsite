11ca212dc4322e5eebfcefab0c4ed6248c6740c9
Michiru Tsuji <tsujim@seattleu.edu>
2023-05-17 16:22:50 -0700

    Modified unit test in ProductList.razor.Tests.cs

--- UnitTests/Components/ProductList.razor.Tests.cs
+++ UnitTests/Components/ProductList.razor.Tests.cs
@@ -19,12 +19,12 @@ namespace UnitTests.Components
 
         #endregion TestSetup
 
-        #region DefaultProductList
+        #region FeaturedProductList
         /// <summary>
-        /// A unit test to verify the default product list exists
+        /// A unit test to verify the featured product list exists
         /// </summary>
         [Test]
-        public void ProductList_Default_Should_Return_Content()
+        public void ProductList_Featured_Should_Return_Content()
         {
             // Arrange
             Services.AddSingleton<JsonFileProductService>(TestHelper.ProductService);
@@ -38,66 +38,29 @@ namespace UnitTests.Components
             // Assert
             Assert.AreEqual(true, result.Contains("Wolfie"));
         }
-        #endregion DefaultProductList
+        #endregion FeaturedProductList
 
         #region SubmitRating
         [Test]
-        public void SubmitRating_Valid_ID_Click_Unstarred_Should_Increment_Count_And_Check_Star()
+        public void SubmitRating_ValidID_ClickUnstarred_ShouldIncrementCount_AndCheckStar()
         {
-            // Arrange
             Services.AddSingleton<JsonFileProductService>(TestHelper.ProductService);
             var id = "MoreInfoButton_2";
 
             var page = RenderComponent<ProductList>();
 
-            // Find the button (more info)
             var buttonList = page.FindAll("Button");
 
-            // Find the one that matches the ID looking for and click it
-            var button = buttonList.FirstOrDefault(m => m.OuterHtml.Contains(id));
+            var button = buttonList.First(m => m.OuterHtml.Contains(id));
             button.Click();
 
-            // Get the markup of the page post-click
             var buttonMarkup = page.Markup;
 
-            // Get the star button
             var starButtonList = page.FindAll("span");
 
-            // Get the vote count
-            var preVoteCountSpan = starButtonList[1];
-            var preVoteCountString = preVoteCountSpan.OuterHtml;
+            var preVoteCountSpan = starButtonList[2];
+            var preVoteCountStr
 
-            // Get the first star item from the list, it should not be checked
-            var starButton = starButtonList.FirstOrDefault(m => !string.IsNullOrEmpty(m.ClassName) && m.ClassName.Contains("fa fa-star"));
-
-            // Save the html for it to compare to after the click
-            var preStarChange = starButton.OuterHtml;
-
-            // Act
-            // Click the star button
-            starButton.Click();
-
-            // Get the markup to use for the assert
-            buttonMarkup = page.Markup;
-
-            // Get the star button
-            starButtonList = page.FindAll("span");
-
-            // Get the vote count
-            var postVoteCountSpan = starButtonList[1];
-            var postVoteCountString = postVoteCountSpan.OuterHtml;
-
-            // Get the last starred item from the list
-            starButton = starButtonList.First(m => !string.IsNullOrEmpty(m.ClassName) && m.ClassName.Contains("fa fa-star checked"));
-
-            // Save the html to compare after the click
-            var postStarChange = starButton.OuterHtml;
-
-            // Assert
-            // Confirm that the record had no votes to start, and 1 vote after
-            Assert.AreEqual(true, preVoteCountString.Contains("Be the first to vote!"));
-            Assert.AreEqual(true, postVoteCountString.Contains("1 Vote"));
-            Assert.AreEqual(false, preVoteCountString.Equals(postVoteCountString));
         }
         #endregion SubmitRating
 
