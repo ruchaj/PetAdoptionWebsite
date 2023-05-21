814aa2af41ee812030de3f5c5d15217de725b076
lesli <ug@fontbonne.edu>
2023-05-20 23:49:01 -0700

    Added unit test to 404.cshtml.Tests.cs to increase code coverage to 100%

---
+++ UnitTests/Pages/Errors/404.cshtml.Tests.cs
@@ -0,0 +1,35 @@
+﻿using ContosoCrafts.WebSite.Pages;
+using ContosoCrafts.WebSite.Pages.Errors;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Pages.Errors
+{
+    public class _404
+    {
+        #region OnGet
+        /// <summary>
+        /// A class to test OnGet function
+        /// </summary>
+        [Test]
+        public void OnGet_Valid_Should_Return_Valid_State()
+        {
+            // Arrange
+            var model = new _404Model();
+
+            // Act
+            model.OnGet();
+
+            // Assert
+            Assert.AreEqual(true, model.ModelState.IsValid);
+            
+        }
+        #endregion OnGet
+
+    }
+}
