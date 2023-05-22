3c87be5f55338b81c2ed13f74c941af38d47ad6e
ruchaj <ruchajrocks@gmail.com>
2023-05-21 18:12:03 -0700

    Creates the page CommentModel.cshtml.Tests.cs

---
+++ UnitTests/Models/CommentModel.cshtml.Tests.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using System.Text.Json;
+
+namespace ContosoCrafts.WebSite.Models.Tests
+{
+    /// <summary>
+    /// A class to test CommentModel.
+    /// </summary>
+    public class CommentModelTests
+    {
+        /// <summary>
+        /// Checks if the comment has a set ID
+        /// </summary>
+        [Test]
+        public void Id_ShouldGenerateNonEmptyString()
+        {
+            // Arrange
+            var comment = new CommentModel();
+
+            // Assert
+            Assert.IsFalse(string.IsNullOrEmpty(comment.Id));
+        }
+        /// <summary>
+        /// Checks if the comment(s) has/have a unique ID
+        /// </summary>
+        [Test]
+        public void Id_ShouldGenerateUniqueIdentifiers()
+        {
+            // Arrange
+            var comment1 = new CommentModel();
+            var comment2 = new CommentModel();
+
+            // Assert
+            Assert.AreNotEqual(comment1.Id, comment2.Id);
+        }
+        /// <summary>
+        /// Checks if changing the id updates the id through setting
+        /// </summary>
+        [Test]
+        public void Id_Set_ShouldUpdateIdValue()
+        {
+            // Arrange
+            var comment = new CommentModel();
+            var newId = "new-id";
+
+            // Act
+            typeof(CommentModel).GetProperty("Id").SetValue(comment, newId);
+
+            // Assert
+            Assert.AreEqual(newId, comment.Id);
+        }
+
+    }
+}
