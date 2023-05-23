0c20f20bdf5b8481d4a57ffb4b47d83bceccf70c
lesli <ug@fontbonne.edu>
2023-05-22 21:39:52 -0700

    Added more enum variable to ProductTypeEnum.cs

---
+++ src/Models/ProductTypeEnum.cs
@@ -0,0 +1,29 @@
+﻿namespace ContosoCrafts.WebSite.Models
+{
+    public enum ProductTypeEnum
+    {
+        Undefined = 0,
+        Fish = 1,
+        Other = 5,
+        Cat = 130,
+        Dog = 55,
+    }
+
+    public static class ProductTypeEnumExtensions
+    {
+        public static string DisplayName(this ProductTypeEnum data)
+        {
+            return data switch
+            {
+                ProductTypeEnum.Fish => "Small Freshwater, Large Saltwater",
+                ProductTypeEnum.Other => "Other Pets",
+                ProductTypeEnum.Cat => "Cats",
+                ProductTypeEnum.Dog => "Dogs",
+
+                // Default, Unknown
+                _ => "",
+            };
+        }
+    }
+
+}
