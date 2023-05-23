69665ba4651def98d6cba1ea2743ff9a783ee990
lesli <ug@fontbonne.edu>
2023-05-22 21:57:25 -0700

    Added test cases to get 100% code coverage. Fixed "aborting test case" error in the project.

---
+++ UnitTests/Models/ProductTypeEnum.chstml.Tests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using ContosoCrafts.WebSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Models
+{
+    public class ProductTypeEnumTests
+    {
+        [TestCase(ProductTypeEnum.Fish, "Small Freshwater, Large Saltwater")]
+        [TestCase(ProductTypeEnum.Other, "Other Pets")]
+        [TestCase(ProductTypeEnum.Cat, "Cats")]
+        [TestCase(ProductTypeEnum.Dog, "Dogs")]
+        [TestCase(ProductTypeEnum.Undefined, "")] // Test the default case
+
+        public void DisplayName_ReturnsCorrectDisplayName(ProductTypeEnum data, string expectedDisplayName)
+        {
+            // Arrange
+
+            // Act
+            string actualDisplayName = data.DisplayName();
+
+            // Assert
+            Assert.AreEqual(expectedDisplayName, actualDisplayName);
+        }
+    }
+}
