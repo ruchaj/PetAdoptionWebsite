d83bb3d453cba435452d4082d42c6f18a28fc141
lesli <ug@fontbonne.edu>
2023-05-24 23:07:41 -0700

    Modified logic OnGet() in PetInformation.cshtml.cs

--- src/Pages/Product/PetInformation.cshtml.cs
+++ src/Pages/Product/PetInformation.cshtml.cs
@@ -13,6 +13,19 @@ namespace ContosoCrafts.WebSite.Pages.Product
     /// </summary>
     public class PetInformation : PageModel
     {
+
+        /// <summary>
+        /// Defines Password for PetInformation 
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        ///Get the user's input for PetInformation 
+        /// </summary>
+        [BindProperty(Name = "password", SupportsGet = true)]
+        public string UserInput { get; set; }
+
+
         //Data middle tier
         public JsonFileProductService ProductService { get; }
 
@@ -32,17 +45,22 @@ namespace ContosoCrafts.WebSite.Pages.Product
         /// Get request
         /// </summary>
         /// <param name="id"></param>
-        public IActionResult OnGet(string id)
+        public IActionResult OnGet(string id, string password)
         {
             //Take in product id
             Product = ProductService.GetProducts().FirstOrDefault(m => m.Id.Equals(id));
-            if (Product == null)
+            if (Product == null && Password==null)
             {
                 return RedirectToPage("/InvalidItem");
             }
+            Password = password;
+            Debug.WriteLine(id+ " "+Password);
 
             return Page();
         }
-        
+
+
+     
+
     }
 }
