852c094430b9f894800db6b91429cd35559e4b24
lesli <ug@fontbonne.edu>
2023-05-31 15:57:27 -0700

    Added comments to 404.cshtml.cs

--- src/Pages/Errors/404.cshtml.cs
+++ src/Pages/Errors/404.cshtml.cs
@@ -5,8 +5,12 @@ namespace ContosoCrafts.WebSite.Pages.Errors
 {
     public class _404Model : PageModel
     {
+        /// <summary>
+        /// A class to handle OnGet() .
+        /// </summary>
         public void OnGet()
         {
+            //nothing to do here
         }
     }
 }
